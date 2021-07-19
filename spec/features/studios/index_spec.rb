6651b152b01a2216b2e37dc91692a10cf06a5a71
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-07-19 11:40:49 -0400

    Create spec tests for displaying movie titles on studio index page

--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -11,4 +11,17 @@ RSpec.describe 'The Studio Index Page' do
     expect(page).to have_content(studio_1.location)
     expect(page).to have_content(studio_2.location)
   end
+
+  it 'displays the titles of all the studios movies' do
+    studio_1 = Studio.create!(name: 'Universal Studios', location: 'Orlando')
+    studio_2 = Studio.create!(name: 'Warner Brothers', location: 'Hollywood')
+    movie_1 = studio_1.movies.create!(title: 'Blow', creation_year: 2003, genre: 'Drama')
+    movie_2 = studio_1.movies.create!(title: 'Jurassic Park', creation_year: 1995, genre: 'Drama')
+    movie_3 = studio_2.movies.create!(title: 'Harry Potter 1', creation_year: 2001, genre: 'Drama')
+    visit '/studios'
+
+    expect(page).to have_content(movie_1.title)
+    expect(page).to have_content(movie_2.title)
+    expect(page).to have_content(movie_3.title)    
+  end
 end
