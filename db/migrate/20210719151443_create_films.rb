a52cc78cb8b7f5f05434b11270d9040057019e3e
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-07-19 11:19:53 -0400

    Create migrations and update schema

---
+++ db/migrate/20210719151443_create_films.rb
@@ -0,0 +1,10 @@
+class CreateFilms < ActiveRecord::Migration[5.2]
+  def change
+    create_table :films do |t|
+      t.references :actor, foreign_key: true
+      t.references :movie, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
